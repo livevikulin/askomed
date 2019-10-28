cf5c68726b05094ba5f14832e0360dc32c2a8c98
Виталя Викулин <livevikulin@gmail.com>
2019-10-28 13:45:48 +0700

    исправил баг с крестиками

--- src/blocks/modules/footer/footer.js
+++ src/blocks/modules/footer/footer.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
 
+	let $open = $('.footer-menu__item svg');
+	
 	$('.js-footer').on('click', function(e) {
 		e.preventDefault();
 		$(this).next().slideToggle();
+		$(this).find($open).toggleClass('footer-active');
 	})
 
 });
