ac6e974d2e15c3e47dd347e35f986cab7431c7c4
Виталя Викулин <livevikulin@gmail.com>
2019-10-31 09:58:26 +0700

    добавил выпадашку выбора города в мобильном меню

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -2,10 +2,12 @@ $(document).ready(function() {
 	
 	let mobileNav = $('.mobile-nav');
 	let _body = $('body');
+	
 	$('.burger').on('click', function() {
 		mobileNav.addClass('menu-active');
 		_body.css('overflow', 'hidden');
 	});
+	
 	$('.close').on('click', function() {
 		mobileNav.removeClass('menu-active');
 		_body.css('overflow', 'auto');
@@ -14,5 +16,18 @@ $(document).ready(function() {
 	$('.nav__link').on('click', function() {
 		$(this).children().toggleClass('rotate');
 		$(this).next().slideToggle();
+	});
+	
+	let $thisCity = $('.city__this');
+	let $otherCity = $('.city__block');
+	
+	$thisCity.on('click', function() {
+		$otherCity.slideToggle();
+		$('.city__choice').toggleClass('city-back');
+	});
+	
+	$('.city__other').on('click', function() {
+		$(this).parent().slideUp();
+		$('.city__choice').removeClass('city-back');
 	});
 });
